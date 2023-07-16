9d3b727a0dfedc22e6e7afb011a626dd376c37b5
Arshpreet Singh <99662930+arshpreet8051@users.noreply.github.com>
2023-07-16 14:12:27 +0530

    Update Support_vector_regression.py

--- Support_vector_regression.py
+++ Support_vector_regression.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 # Importing the dataset
 
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('Data.csv') # 👈🏻 change your file name here
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
